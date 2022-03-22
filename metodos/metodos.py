1d84f460d439acf2a43bbe6b5bd39ba9f5c8c958
jjsanchezc <jjsanchezc@eafit.edu.co>
2022-03-22 13:14:10 -0500

    commit on "master"

--- metodos/metodos.py
+++ metodos/metodos.py
@@ -70,9 +70,37 @@ class metodos:
 
     
     def puntoFijo():
+        """
+            -
+            
+        """
         return 0
 
     def reglaFalsa():
+        """
+            * significa multiplicando
+            -calcular la interseccion con el eje x
+            -hallar la pendiente de la recta
+            -m=(f(b)-f(a))/b-a
+            -hallamos la pendiente de la recta que queda desde el 
+            intercepto hasta el extremo del intervalo en donde la 
+            f cambia de signo
+            -igualando las ecuaciones de ambas pendientes (porque 
+            son triangulos semenjantes) resulta en 
+            -     x= (b-f(b)*(f(b-a)))/f(b)-f(a)
+            -se evalua f(a)*f(x) si es <0 quiere decir que la f
+            sigue teniendo intercepto en el eje x entonces la "x"
+            se convertiria en la nueva b y se repite el proceso
+            de lo contrario
+            - f(b)*f(x) si <0 la "x" se renombrará y será la nueva
+            "a" y se repetirá el proceso
+            -en algun momneto del proceso,la recta que trazaremos 
+            entre las imágenes de los intervalos será igual a la 
+            función a la que le estamos buscando la raíz, allí
+            encontramos la "x"
+            -cuando f(a)*f(x)=0, f(b)*f(x)=0 o x=0 quiere decir que 
+            esa "x" es la raiz que buscamos
+        """
         return 0
     
     def secante():
